c2a952ef1b46bba8d7ff974d04d587f462db3b0a
nobino <nobino_github@outlook.com>
2012-11-10 18:38:34 +0900

    finished lesson #04

---
+++ ruby_lessons/04/hello.rb
@@ -0,0 +1,11 @@
+# varible name should start with lower-case or "_".
+# error, using keyword.
+# end = 10 
+x = 10
+p x
+
+# const name should use only upper-case.
+# separated by "_".
+MY_EMAIL = "hogehoge"
+# warning, constant is cahnged.
+MY_EMAIL = "fugafuga"
