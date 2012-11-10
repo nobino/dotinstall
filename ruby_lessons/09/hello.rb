d54fbc4305dcc356e7acf711eafccd273c598385
nobino <nobino_github@outlook.com>
2012-11-10 20:29:21 +0900

    finished lesson #09

---
+++ ruby_lessons/09/hello.rb
@@ -0,0 +1,16 @@
+# encoding: utf-8
+
+# String Object
+
+x = "hello" # double quoted. evaluates vars and meta charactors
+y = 'hello' # single quoted. not evaluates.
+
+puts 'hello\nworld'
+
+name = "nobino"
+
+puts "my name is #{name}"
+
+# Japanese
+# needs specify encoding at the first line to display.
+puts "こんにちは"
