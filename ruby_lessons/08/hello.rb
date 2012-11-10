136ad8a94fe0c23f4c2c60cf920de7788f8d78f6
nobino <nobino_github@outlook.com>
2012-11-10 20:20:32 +0900

    finished lesson 08

---
+++ ruby_lessons/08/hello.rb
@@ -0,0 +1,16 @@
+# methods of numerical object
+
+# methods
+p "10".to_i # to methods
+p 10.5.to_r # to rational
+
+p 10.8.round
+p 10.8.ceil
+p 10.8.floor
+
+puts "10 random numers."
+for i in 1..10 do
+  p rand(100)
+end
+
+p Math.sqrt(2)
