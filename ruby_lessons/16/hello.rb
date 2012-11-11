799f1f9a1106a95713994af1d4f08eb4014e5643
nobino <nobino_github@outlook.com>
2012-11-11 22:14:14 +0900

    finished lesson #17

--- ruby_lessons/16/hello.rb
+++ ruby_lessons/16/hello.rb
@@ -5,3 +5,15 @@ sales = {"tanaka"=>100, "yamada"=>150, "nagata"=>300}
 p sales
 
 p sales["tanaka"]
+
+# methods of hash object
+p sales.size
+p sales.length
+
+p sales.empty?
+
+p sales.has_key?("tanaka")
+p sales.has_key?("not exists")
+
+p sales.has_value?(100)
+p sales.has_value?(10000)
