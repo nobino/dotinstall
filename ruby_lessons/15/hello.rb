68270f4d16fe619196580755900e8179e90a4916
nobino <nobino_github@outlook.com>
2012-11-11 13:08:29 +0900

    finished lesson #15

---
+++ ruby_lessons/15/hello.rb
@@ -0,0 +1,16 @@
+a = [1, 3, 6, 9, 1, 5, 7]
+
+p a.uniq # remove duplicatues
+
+p a.sort
+
+# p a.sort!
+
+# first / last
+# unshift / push (add)
+# shift / pop (delete)
+
+p a.push(10)
+p a.pop()
+p a.unshift(0)
+p a.shift
