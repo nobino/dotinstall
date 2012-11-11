6650cd3b397ea888d59240f3fc0c939ec88e61d2
nobino <nobino_github@outlook.com>
2012-11-11 23:04:05 +0900

    finished lesson #22

---
+++ ruby_lessons/22/hello.rb
@@ -0,0 +1,14 @@
+# comparation operator
+# < > <= >=
+
+# == !=
+
+# =~ Regular Expression
+puts "match!" if /nobino/ =~ "my name is nobino"
+
+# && ||
+
+x = 5
+if x >= 1 && x < 10
+  puts "true"
+end
