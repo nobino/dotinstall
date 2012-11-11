6328d53481d501f15e7b60724e6d68e85ff5decb
nobino <nobino_github@outlook.com>
2012-11-11 12:55:24 +0900

    finished lesson #13

--- ruby_lessons/12/hello.rb
+++ ruby_lessons/12/hello.rb
@@ -13,3 +13,19 @@ p names[1..2]
 p names[0...2] # exclude 2
 
 p names[-1] # access [length - 1]
+
+names[0] = "taro_new"
+
+p names[0]
+
+names[1..2] = ["jiro_new", "hanako_new"]
+
+p names
+
+names[1, 2] = ["a", "b"]
+
+p names
+
+names [1, 0] = ["c", "d"] # insertion
+
+p names
