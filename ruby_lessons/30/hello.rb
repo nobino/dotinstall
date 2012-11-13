73e7935c2cf8dcba88bac65b266e17702faa6453
nobino <nobino_github@outlook.com>
2012-11-13 10:13:08 +0900

    add a init param to initialize(), fix a typo in the damage()

--- ruby_lessons/30/hello.rb
+++ ruby_lessons/30/hello.rb
@@ -1,13 +1,13 @@
 # Class
 
 class Monster # start with upper-case
-  def initialize # constructor in Java
+  def initialize(name) # constructor in Java
     @name = name # instance var starts with @
     @hp = 100 + rand(100)
     printf("%s appeared. His hp is %d\n", @name, @hp)
   end
 
-  def damege
+  def damage
     @hp -= 10 + rand(10)
     printf("%s's hp is now %d\n", @name, @hp)
     printf("%s is now dead!\n", @name) if @hp < 0
