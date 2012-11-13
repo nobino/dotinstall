6e5dc5a8e313bc1ebc110e8dff058d4e4a0b780f
nobino <nobino_github@outlook.com>
2012-11-13 11:59:38 +0900

    finished lesson #10

---
+++ railsproject/blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '8ef02285da51922934bf7278660478655abfa01f033aa597f2c4f2aa3f7c181a58df701836509fb92689d7079449984cccdd7377bf0412fe939e9861f4c02266'
