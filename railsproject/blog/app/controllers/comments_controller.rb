bd9888367421a0dd3cd4b739369082620f773b8b
nobino <nobino_github@outlook.com>
2012-11-22 00:15:29 +0900

    implemented comment create fuinction.
    finished lesson #37
    
    finished lesson #38
    
    finished lesson #39
    
    finished lesson #40
    
    finished lesson #41

---
+++ railsproject/blog/app/controllers/comments_controller.rb
@@ -0,0 +1,9 @@
+class CommentsController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = Post.find(params[:post_id]).comments.create(params[:comment])
+    redirect_to post_path(@post)
+  end
+
+end
