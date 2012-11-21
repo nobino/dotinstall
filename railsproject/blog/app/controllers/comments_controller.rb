7328acec05853b8aced62a7364c6106976d4368b
nobino <nobino_github@outlook.com>
2012-11-22 01:29:37 +0900

    finished lesson #42. add validations to comment form.

--- railsproject/blog/app/controllers/comments_controller.rb
+++ railsproject/blog/app/controllers/comments_controller.rb
@@ -2,8 +2,12 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = Post.find(params[:post_id]).comments.create(params[:comment])
-    redirect_to post_path(@post)
+    @comment = Post.find(params[:post_id]).comments.new(params[:comment])
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      render :template => "posts/show"
+    end
   end
 
 end
