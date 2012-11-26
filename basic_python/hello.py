d2bb1545c98ed259a33fdb6c62229ec3e02a3b14
nobino <nobino_github@outlook.com>
2012-11-26 12:15:32 +0900

    finished lesson #05.

--- basic_python/hello.py
+++ basic_python/hello.py
@@ -3,3 +3,23 @@
 
 msg = 'hello world'
 print msg
+
+# 数値(整数、小数、複素数)
+# 演算子 + - * /
+print 1 + 2
+print 5 - 2
+print 2 * 1.5
+print 3.0 / 2
+
+# modulo %
+print 10 % 3
+
+# power **
+print 2**10
+
+# 整数どうしの割り算は、小数点以下切り捨ての整数を返す
+print 10 / 3 # 3
+print 10 / 3.0 # 3.333...
+
+# 小数を含む割り算でも、小数点以下を切り捨てたいときは、 //
+print 10 // 3 # 3
