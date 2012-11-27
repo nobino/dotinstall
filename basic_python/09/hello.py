282ac5f518bc39624e6103c02f14ac3876a6adba
nobino <nobino_github@outlook.com>
2012-11-28 01:19:03 +0900

    finished lesson #09

---
+++ basic_python/09/hello.py
@@ -0,0 +1,14 @@
+# coding: UTF-8
+# COnversion Stirng and number
+
+# ERROR!
+# print 5 + '5'
+
+print 5 + int('5')
+print 5 + float('3.5')
+
+age = 20
+
+# ERROR!
+# print 'I am' + age + 'years old'
+print 'I am ' + str(age) + ' years old'
