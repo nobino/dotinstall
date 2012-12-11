17144a0cdfe3eac574f7c372ca9af4ed35dc7639
nobino <nobino_github@outlook.com>
2012-12-12 00:21:53 +0900

    finished lesson #18, #19 about if statement.

---
+++ basic_python/18/hello.py
@@ -0,0 +1,19 @@
+# coding: UTF-8
+
+score = 50
+if score >= 80:
+    print 'pass!'
+else:
+    print 'not pass!'
+
+print 'pass!' if score >= 80 else 'not pass!'
+
+if score >= 80:
+    print 'OK!'
+elif score >= 60:
+    print 'so so'
+else:
+    print 'oh...'
+
+if 40 < score < 60:
+    print '...'
