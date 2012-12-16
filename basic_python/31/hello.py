b164ed20443cf9fb37ee20b64c02ffafff7bf5c6
nobino <nobino_github@outlook.com>
2012-12-16 22:40:05 +0900

    finished lesson #31.

---
+++ basic_python/31/hello.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+# use module
+import math, random
+from datetime import date
+
+print math.pi
+
+print math.ceil(5.2)
+print math.floor(5.2)
+
+print random.random()
+
+print date.today()
